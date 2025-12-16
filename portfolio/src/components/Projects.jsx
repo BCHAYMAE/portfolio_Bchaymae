121884e3f372c8526fad9a6667ba49ef49e030d6
BCHAYMAE <bellahcene.chaymae@gmail.com>
2025-12-16 22:17:06 +0100

    still

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,9 +1,61 @@
-import "./Projects.css";
+import "./Projects.css"
+import pro1 from "../assets/pro1.png"
 
 export default function Projects() {
+  const projects = [
+    {
+      title: "GitRepo AutoDeploy App",
+      description:
+        "This application is a full-stack deployment tool that automates cloning Git repositories, auto-detecting frontend/backend/database technologies, generating Dockerfiles, and deploying containerized apps via Docker Compose with real-time WebSocket updates.",
+      tags: [
+        "Node.js",
+        "Express.js",
+        "React-Vite",
+        "Socket.IO",
+        "simple-git",
+        "CORS",
+        "Docker",
+      ],
+      image: pro1, 
+      repo: "https://github.com/BCHAYMAE/deployment_app",
+    },
+  ]
+
   return (
-    <div className="projects-wrapper">
-      <h2>Featured Projects</h2>
-    </div>
-  );
+    <section id="projects" className="projects">
+      <div className="projects-container">
+        <div className="projects-header">
+          <h2>Featured Projects</h2>
+          <p>A selection of work I'm proud of</p>
+        </div>
+
+        <div className="projects-grid">
+          {projects.map((project, idx) => (
+            <div key={idx} className="project-card">
+              <div className="project-image">
+                <img src={project.image} alt={project.title} />
+              </div>
+
+              <div className="project-content">
+                <h3>
+                  <a href={project.repo} target="_blank"rel="noopener noreferrer" className="project-link">
+                  {project.title}
+                  </a></h3>
+                <p>{project.description}</p>
+
+                <div className="project-tags">
+                  {project.tags.map((tag, tagIdx) => (
+                    <span key={tagIdx} className="tag">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
+
